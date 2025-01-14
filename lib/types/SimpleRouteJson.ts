d2de47a314e6b1a8aebf5848ba729bfe87a13d04
seveibar <seveibar@gmail.com>
2025-01-13 20:46:35 -0800

    format

--- lib/types/SimpleRouteJson.ts
+++ lib/types/SimpleRouteJson.ts
@@ -1,55 +1,55 @@
 export interface RouteWire {
-	route_type: "wire";
-	x: number;
-	y: number;
-	width: number;
-	layer: string;
+  route_type: "wire"
+  x: number
+  y: number
+  width: number
+  layer: string
 }
 
 export interface RouteVia {
-	route_type: "via";
-	from_layer: string;
-	to_layer: string;
-	x: number;
-	y: number;
+  route_type: "via"
+  from_layer: string
+  to_layer: string
+  x: number
+  y: number
 }
 
 export interface SimplifiedPcbTrace {
-	route: Array<RouteWire | RouteVia>;
+  route: Array<RouteWire | RouteVia>
 }
 
 export type Obstacle = {
-	type: "rect";
-	layers: string[];
-	center: { x: number; y: number };
-	width: number;
-	height: number;
-	connectedTo: string[];
-};
+  type: "rect"
+  layers: string[]
+  center: { x: number; y: number }
+  width: number
+  height: number
+  connectedTo: string[]
+}
 
 export interface PointWithLayer {
-	x: number;
-	y: number;
-	layer: string;
-	pcb_port_id?: string;
+  x: number
+  y: number
+  layer: string
+  pcb_port_id?: string
 }
 
 export interface SimpleRouteConnection {
-	name: string;
-	pointsToConnect: Array<PointWithLayer>;
+  name: string
+  pointsToConnect: Array<PointWithLayer>
 }
 
 export interface ConnectionWithGoalAlternatives extends SimpleRouteConnection {
-	startPoint: PointWithLayer;
-	endPoint: PointWithLayer;
-	goalBoxes: Obstacle[];
+  startPoint: PointWithLayer
+  endPoint: PointWithLayer
+  goalBoxes: Obstacle[]
 }
 
 export interface SimpleRouteJson {
-	layerCount: number;
-	minTraceWidth: number;
-	obstacles: Obstacle[];
-	connections: Array<SimpleRouteConnection | ConnectionWithGoalAlternatives>;
-	bounds: { minX: number; maxX: number; minY: number; maxY: number };
-	traces?: SimplifiedPcbTrace[];
+  layerCount: number
+  minTraceWidth: number
+  obstacles: Obstacle[]
+  connections: Array<SimpleRouteConnection | ConnectionWithGoalAlternatives>
+  bounds: { minX: number; maxX: number; minY: number; maxY: number }
+  traces?: SimplifiedPcbTrace[]
 }
